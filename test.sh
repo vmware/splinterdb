74f68182faac09f44409063fbd99e60c12540bfa
Alex Conway <aconway@vmware.com>
2021-08-01 19:28:23 -0700

    Adding splinter_test --perf to CI

--- test.sh
+++ test.sh
@@ -9,6 +9,8 @@ DRIVER="${DRIVER:-"${BINDIR:-bin}/driver_test"}"
 
 "$DRIVER" splinter_test --functionality 1000000 100 --seed "$SEED"
 
+"$DRIVER" splinter_test --perf --max-async-inflight 0 --num-insert-threads 4 --num-lookup-threads 4 --num-range-lookup-threads 0 --tree-size-gib 2 --cache-capacity-mib 512
+
 "$DRIVER" cache_test --seed "$SEED"
 
 "$DRIVER" btree_test --seed "$SEED"
