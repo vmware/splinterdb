96368a5d4843ebe93bf99a3f51581d328e0b932c
Gabriel Rosenhouse <grosenhouse@vmware.com>
2021-08-17 04:49:24 +0000

    Add iterator to kvstore
    Also: simplify kvstore opaque type

--- src/kvstore.h
+++ src/kvstore.h
@@ -4,7 +4,12 @@
 /*
  * kvstore.h --
  *
- *     This file contains the external kvstore interfaces based on splinterdb
+ *     This file contains the external kvstore interfaces
+ *     based on splinterdb.
+ *     The user must provide a data_config that encodes
+ *     values and message-types.
+ *
+ *     For a simpler API, look at splinter_simple.h
  */
 
 #ifndef _KVSTORE_H_
@@ -23,21 +28,98 @@ typedef struct kvstore_config {
    void *heap_id;
 } kvstore_config;
 
-typedef struct kvstore *kvstore_handle;
+typedef struct kvstore kvstore;
 
-int kvstore_init(const kvstore_config *cfg, kvstore_handle *h);
+// Initialize a kvstore from a given config
+//
+// The library will allocate and own the memory for kvstore
+// and will free it on kvstore_deinit
+int kvstore_init(const kvstore_config *cfg, kvstore **kvs);
 
-void kvstore_deinit(kvstore_handle h);
+void kvstore_deinit(kvstore *kvs);
 
-void kvstore_register_thread(const kvstore_handle h);
+void kvstore_register_thread(const kvstore *kvs);
 
-// FIXME: key/value can't be marked const until splinter API's are fixed
-int kvstore_insert(const kvstore_handle h, char *key, char *message);
+int kvstore_insert(const kvstore *kvs, char *key, char *message);
 
-int kvstore_lookup(const kvstore_handle h,       // IN
-                   char *               key,     // OUT
-                   char *               message, // OUT
-                   bool *               found    // OUT
+int kvstore_lookup(const kvstore *kvs,     // IN
+                   char *         key,     // IN
+                   char *         message, // OUT
+                   bool *         found    // OUT
 );
+
+/*
+KVStore Iterator API
+
+This API is modeled after the RocksDB Iterator and is intended to allow drop-in
+replacement for most scenarios.
+
+This documentation is heavily inspired by
+  https://github.com/facebook/rocksdb/wiki/Iterator
+
+The biggest difference is that RocksDB's Iterator exposes a Seek(key) method,
+while SplinterDB expects the starting key to be provided at the time the
+iterator is initialized.
+
+Similar to RocksDB, if there is no error, then status()==0.  If status() != 0,
+then valid() == false.  In other words, valid()==true implies status()== 0,
+which means it is safe to proceed with other operations without checking
+status().
+
+On the other hand, if valid() == false, then there are two possibilities:
+(1) We reached the end of the data. In this case, status() == 0;
+(2) there is an error. In this case status() != 0;
+It is always a good practice to check status() if the iterator is invalidated.
+
+Sample application code:
+
+   kvstore_iterator* it;
+   int rc = kvstore_iterator_init(kvs, &it, NULL);
+   if (rc != 0) { ... handle error ... }
+
+   char* key;
+   char* msg;
+   for(; kvstore_iterator_valid(it); kvstore_iterator_next(it)) {
+      rc = kvstore_iterator_get_current(it, &key, &msg);
+      if (rc != 0) { ... handle error ... }
+      // read key and msg ...
+   }
+   // loop exit may mean error, or just that we've reached the end of the range
+   rc = kvstore_iterator_status(it);
+   if (rc != 0) { ... handle error ... }
+*/
+
+typedef struct kvstore_iterator kvstore_iterator;
+
+int kvstore_iterator_init(const kvstore *    kvs,      // IN
+                          kvstore_iterator **iter,     // OUT
+                          char *             start_key // IN
+);
+
+void kvstore_iterator_deinit(kvstore_iterator *iter);
+
+// checks that the iterator status is OK (no errors) and that get_current will
+// succeed If false, there are two possibilities:
+// 1. Iterator has passed the final item.  In this case, status() == 0
+// 2. Iterator has encountered an error.  In this case, status() != 0
+bool kvstore_iterator_valid(kvstore_iterator *iter);
+
+// attempts to advance the iterator to the next item
+// any error will cause valid() == false and be visible with status()
+void kvstore_iterator_next(kvstore_iterator *iter);
+
+// Sets *key and *message to the locations of the current item
+// Callers must not modify that memory
+//
+// If valid() == false, then behavior is undefined.
+void kvstore_iterator_get_current(kvstore_iterator *iter,   // IN
+                                  const char **     key,    // OUT
+                                  const char **     message // OUT
+);
+
+// Returns an error encountered from iteration, or 0 if successful.
+//
+// End-of-range is not an error
+int kvstore_iterator_status(const kvstore_iterator *iter);
 
 #endif // _KVSTORE_H_
--- tests/kvstore_test.c
+++ tests/kvstore_test.c
@@ -19,9 +19,9 @@ kvstore_test(int argc, char *argv[])
 {
    fprintf(stderr, "kvstore_test: starting\n");
 
-   int            kv_result;
-   kvstore_handle kvs_handle = {0};
-   kvstore_config kvs_cfg    = {0};
+   int            rc;
+   kvstore_config kvs_cfg = {0};
+   kvstore *      kvs;
 
    kvs_cfg.filename   = "db";
    kvs_cfg.cache_size = Giga;      // see config.c: cache_capacity
@@ -29,107 +29,198 @@ kvstore_test(int argc, char *argv[])
 
    kvs_cfg.data_cfg = *test_data_config;
 
-   kv_result = kvstore_init(&kvs_cfg, &kvs_handle);
-   if (kv_result != 0) {
-      fprintf(stderr, "kvstore_init error: %d\n", kv_result);
+   rc = kvstore_init(&kvs_cfg, &kvs);
+   if (rc != 0) {
+      fprintf(stderr, "kvstore_init error: %d\n", rc);
       return -1;
    }
 
-   kvstore_register_thread(kvs_handle);
+   kvstore_register_thread(kvs);
 
    fprintf(stderr, "kvstore_test: initializing test data\n");
-   char *key   = calloc(1, kvs_cfg.data_cfg.key_size);
-   char *value = calloc(1, kvs_cfg.data_cfg.message_size);
-   if (value == NULL) {
-      fprintf(stderr, "calloc value buffer\n");
+   char *key        = calloc(1, kvs_cfg.data_cfg.key_size);
+   char *msg_buffer = calloc(1, kvs_cfg.data_cfg.message_size);
+   if (msg_buffer == NULL) {
+      fprintf(stderr, "calloc message buffer\n");
+      goto cleanup;
    }
    bool found;
    memcpy(key, "foo", 3);
 
    fprintf(stderr, "kvstore_test: lookup non-existent key...");
-   kv_result = kvstore_lookup(kvs_handle, key, value, &found);
-   if (kv_result != 0) {
-      fprintf(stderr, "kvstore_lookup: %d\n", kv_result);
+   rc = kvstore_lookup(kvs, key, msg_buffer, &found);
+   if (rc != 0) {
+      fprintf(stderr, "kvstore_lookup: %d\n", rc);
       goto cleanup;
    }
    fprintf(stderr, "found=%d\n", found);
    if (found) {
-      kv_result = -1;
+      rc = -1;
       fprintf(stderr, "unexpectedly found a key we haven't set\n");
       goto cleanup;
    }
 
-   data_handle *valueStruct  = (data_handle *)value;
-   valueStruct->message_type = MESSAGE_TYPE_INSERT;
-   valueStruct->ref_count    = 1;
-   memcpy((void *)(valueStruct->data), "bar", 3);
+   data_handle *msg  = (data_handle *)msg_buffer;
+   msg->message_type = MESSAGE_TYPE_INSERT;
+   msg->ref_count    = 1;
+   memcpy((void *)(msg->data), "bar", 3);
 
-   fprintf(stderr,
-           "inserting key with data = %.*s\n",
-           3,
-           (char *)(valueStruct->data));
-   kv_result = kvstore_insert(kvs_handle, key, value);
-   if (kv_result != 0) {
-      fprintf(stderr, "kvstore_insert: %d\n", kv_result);
+   fprintf(stderr, "inserting key with data = %.*s\n", 3, (char *)(msg->data));
+   rc = kvstore_insert(kvs, key, msg_buffer);
+   if (rc != 0) {
+      fprintf(stderr, "kvstore_insert: %d\n", rc);
       goto cleanup;
    }
 
    // muck with the value, just to see that it gets reset
-   valueStruct->message_type = MESSAGE_TYPE_UPDATE;
-   snprintf((void *)(valueStruct->data),
+   msg->message_type = MESSAGE_TYPE_UPDATE;
+   snprintf((void *)(msg->data),
             kvs_cfg.data_cfg.message_size - offsetof(data_handle, data),
             "zzz");
    fprintf(
       stderr,
       "after insert, we set local variable data = %.*s just to mess it up\n",
       3,
-      (char *)(valueStruct->data));
+      (char *)(msg->data));
 
    fprintf(stderr, "kvstore_test: lookup #2...");
-   kv_result = kvstore_lookup(kvs_handle, key, value, &found);
-   if (kv_result != 0) {
-      fprintf(stderr, "kvstore_lookup: %d\n", kv_result);
+   rc = kvstore_lookup(kvs, key, msg_buffer, &found);
+   if (rc != 0) {
+      fprintf(stderr, "kvstore_lookup: %d\n", rc);
       goto cleanup;
    }
    fprintf(stderr, "found=%d\n", found);
    if (!found) {
-      kv_result = -1;
+      rc = -1;
       fprintf(stderr, "unexpectedly 'found' is false\n");
       goto cleanup;
    }
-   if (memcmp(valueStruct->data, "bar", 3) != 0) {
-      kv_result = -1;
+   if (memcmp(msg->data, "bar", 3) != 0) {
+      rc = -1;
       fprintf(stderr,
               "lookup returned an unexpected value for data = %.*s\n",
               3,
-              (char *)(valueStruct->data));
+              (char *)(msg->data));
    }
 
    fprintf(stderr, "kvstore_test: delete key\n");
-   valueStruct->message_type = MESSAGE_TYPE_DELETE;
-   kv_result                 = kvstore_insert(kvs_handle, key, value);
-   if (kv_result != 0) {
-      fprintf(stderr, "kvstore_insert (for delete): %d\n", kv_result);
+   msg->message_type = MESSAGE_TYPE_DELETE;
+   rc                = kvstore_insert(kvs, key, msg_buffer);
+   if (rc != 0) {
+      fprintf(stderr, "kvstore_insert (for delete): %d\n", rc);
       goto cleanup;
    }
 
    fprintf(stderr, "kvstore_test: lookup #3, for now-deleted key...");
-   kv_result = kvstore_lookup(kvs_handle, key, value, &found);
-   if (kv_result != 0) {
-      fprintf(stderr, "kvstore_lookup: %d\n", kv_result);
+   rc = kvstore_lookup(kvs, key, msg_buffer, &found);
+   if (rc != 0) {
+      fprintf(stderr, "kvstore_lookup: %d\n", rc);
       goto cleanup;
    }
    fprintf(stderr, "found=%d\n", found);
    if (found) {
-      kv_result = -1;
+      rc = -1;
       fprintf(stderr, "unexpectedly 'found' is true\n");
       goto cleanup;
    }
 
+   const int max_val_size = kvs_cfg.data_cfg.message_size - sizeof(data_handle);
 
+   const int num_inserts = 50;
+   msg->message_type     = MESSAGE_TYPE_INSERT;
+   fprintf(stderr, "kvstore_test: inserting %d keys", num_inserts);
+   // insert keys backwards, just for kicks
+   for (int i = num_inserts - 1; i >= 0; i--) {
+      fprintf(stderr, ".");
+      int key_len = snprintf(key, kvs_cfg.data_cfg.key_size, "key-%04d", i);
+      if (!(key_len > 0 && key_len < kvs_cfg.data_cfg.key_size)) {
+         fprintf(stderr, "expected: generated key fits in buffer\n");
+         rc = -1;
+         goto cleanup;
+      }
+
+      int val_len = snprintf((char *)(msg->data), max_val_size, "val-%04d", i);
+      if (!(val_len > 0 && val_len < max_val_size)) {
+         fprintf(stderr, "expected: generated value fits in buffer\n");
+         rc = -1;
+         goto cleanup;
+      }
+
+      rc = kvstore_insert(kvs, key, msg_buffer);
+      if (rc != 0) {
+         fprintf(stderr, "insert failed\n");
+         rc = -1;
+         goto cleanup;
+      }
+   }
+   fprintf(stderr, "\nkvstore_test: inserts done, now using iterator:");
+
+   kvstore_iterator *it;
+   rc = kvstore_iterator_init(kvs, &it, NULL /* start key */);
+   if (rc != 0) {
+      fprintf(stderr, "initializing iterator: %d\n", rc);
+      goto cleanup;
+   }
+
+   // to check that we get not-found at the end
+   const char *current_key;
+   const char *current_msg;
+
+   int i = 0;
+   for (; kvstore_iterator_valid(it); kvstore_iterator_next(it)) {
+      char expected_key[24] = {0};
+      char expected_val[24] = {0};
+      int  expected_key_len =
+         snprintf(expected_key, kvs_cfg.data_cfg.key_size, "key-%04d", i);
+      platform_assert(expected_key_len > 0 &&
+                      expected_key_len < kvs_cfg.data_cfg.key_size);
+      int expected_val_len =
+         snprintf(expected_val, max_val_size, "val-%04d", i);
+      platform_assert(expected_val_len > 0 && expected_val_len < max_val_size);
+
+      kvstore_iterator_get_current(it, &current_key, &current_msg);
+      char *current_val = (char *)(((data_handle *)current_msg)->data);
+
+      if (memcmp(current_key, expected_key, kvs_cfg.data_cfg.key_size) != 0) {
+         fprintf(stderr, "iteration %d: mismatched key\n", i);
+         rc = -1;
+         goto iter_cleanup;
+      }
+
+      if (memcmp(current_val, expected_val, max_val_size) != 0) {
+         fprintf(stderr, "iteration %d: mismatched value\n", i);
+         rc = -1;
+         goto iter_cleanup;
+      }
+      fprintf(stderr, ".");
+      i++;
+   }
+   rc = kvstore_iterator_status(it);
+   if (rc != 0) {
+      fprintf(stderr, "iterator stopped with error status: %d\n", rc);
+      goto iter_cleanup;
+   }
+
+   if (i != num_inserts) {
+      fprintf(stderr, "iterator stopped at %d, expected %d\n", i, num_inserts);
+      rc = -1;
+      goto iter_cleanup;
+   }
+
+   if (kvstore_iterator_valid(it)) {
+      fprintf(stderr, "iterator still valid, this should not happen\n");
+      rc = -1;
+      goto iter_cleanup;
+   }
+   fprintf(stderr, "OK.  iterator test complete\n");
+
+
+iter_cleanup:
+   fprintf(stderr, "\nkvstore_test: de-init iterator\n");
+   kvstore_iterator_deinit(it);
 cleanup:
-   kvstore_deinit(kvs_handle);
-   if (kv_result == 0) {
+   kvstore_deinit(kvs);
+   if (rc == 0) {
       fprintf(stderr, "kvstore_test: succeeded\n");
       return 0;
    } else {
