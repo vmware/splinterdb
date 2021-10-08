01af8d0f9f6efb420c75b34937d78216975348cf
Aditya Gurajada <agurajada@vmware.com>
2021-10-08 18:56:56 +0000

    Set MAX_MESSAGE_SIZE 1900 to w/a BTree page-full assertion.

--- include/splinterdb/limits.h
+++ include/splinterdb/limits.h
@@ -12,6 +12,10 @@
 #define __LIMITS_H__
 
 #define MAX_KEY_SIZE     100
-#define MAX_MESSAGE_SIZE 2000
+
+// We get assertion from BTree code if this limit is 2000.
+// We get value-too-large error if this is 1800, when FDB seemingly inserts
+// values beyond 1800 bytes. 1900 seems to work fine.
+#define MAX_MESSAGE_SIZE 1900
 
 #endif // __LIMITS_H__
