3e99908e783c78a49c8bc1c4831696b331972fc9
Aditya Gurajada <agurajada@vmware.com>
2021-12-03 21:28:13 +0000

    (#167) Customize ctest code to conform to SplinterDB coding styles.
    This commit applies SplinterDB-specific clang-formatting rules.
    - Specifically, as ctest.h has complicated macros defined in it,
      suppress clang-format for this file.
    - Delete C++ code from ctest.h, as we are only doing C-based code/
      test-dev.
    - Minor re-indentation of functions and such like in few different
      files. No ctest code flow logic is changed in this commit.
    - Add --help usage message.

--- ctests/main.c
+++ ctests/main.c
@@ -4,16 +4,23 @@
 
 // uncomment lines below to enable/disable features. See README.md for details
 #define CTEST_SEGFAULT
-//#define CTEST_NO_COLORS
-//#define CTEST_COLOR_OK
+// #define CTEST_NO_COLORS
+// #define CTEST_COLOR_OK
 
 #include "ctest.h"
 
-int main(int argc, const char *argv[])
+int
+main(int argc, const char *argv[])
 {
-    int result = ctest_main(argc, argv);
-
-    printf("\nNOTE: some tests will fail, just to show how ctest works! ;)\n");
-    return result;
+   int result = ctest_main(argc, argv);
+   return result;
 }
 
+/*
+ * Generate brief usage information to run CTests.
+ */
+void
+ctest_usage(const char * progname)
+{
+    printf("%s [ <suite-name> [ <test-case-name> ] ]\n", progname);
+}
