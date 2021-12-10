03441c2af7154731a9fdb8db48dc749868a068fc
Aditya Gurajada <agurajada@vmware.com>
2021-12-10 19:56:29 +0000

    (#171) Rework Makefile rules to build standalone unit-tests
    This commit refactors slightly the rules for building standalone
    unit-tests. To simplify usage, the target will be: unit/<program>;
    i.e. make unit/variable_length_btree-test
    
    Refactored rules & targets in Makefile to now support the
    following:
    
     - Build bin/unit_test and each stand-alone unit-test separately
     - New targets:
        - tests: Will rebuild only the test code
        - unit_test: Will rebuild only the unit-test code and
                each stand-alone unit-test binary
        - run-tests: Existing target 'test' renamed to this
        - test-results: Runs all tests and redirects output to
                a file in current dir named ./test-results.out
    
    Following usages work with these changes:
    
        $ make clean; make
        $ make clean; make tests
        $ make clean; make unit_test
        $ make clean; make unit/variable_length_btree-test
        $ make clean; make unit/kvstore_basic_test

--- test.sh
+++ test.sh
@@ -8,25 +8,39 @@ TEST_RUST="${TEST_RUST:-false}"
 UNIT_TEST_DRIVER="${UNIT_TEST_DRIVER:-"${BINDIR:-bin}/unit_test"}"
 
 # Run all the unit-tests first, to get basic coverage
+echo
 "$UNIT_TEST_DRIVER"
+UNIT_TESTS_DB_DEV="unit_tests_db"
+if [ -f ${UNIT_TESTS_DB_DEV} ]; then
+    rm ${UNIT_TESTS_DB_DEV}
+fi
 
+echo
 "$DRIVER" kvstore_test --seed "$SEED"
 
+echo
 "$DRIVER" kvstore_basic_test --seed "$SEED"
 
+echo
 "$DRIVER" splinter_test --functionality 1000000 100 --seed "$SEED"
 
+echo
 "$DRIVER" splinter_test --perf --max-async-inflight 0 --num-insert-threads 4 --num-lookup-threads 4 --num-range-lookup-threads 0 --tree-size-gib 2 --cache-capacity-mib 512
 
+echo
 "$DRIVER" cache_test --seed "$SEED"
 
+echo
 "$DRIVER" btree_test --seed "$SEED"
 
+echo
 "$DRIVER" log_test --seed "$SEED"
 
+echo
 "$DRIVER" filter_test --seed "$SEED"
 
 "$DRIVER" util_test --seed "$SEED"
+echo
 
 if [ "$TEST_RUST" = "true" ]; then
    pushd rust
