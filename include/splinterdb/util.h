bad7b02d849df979651addb51fb758713b86e58d
Rob Johnson <rob@robjohnson.io>
2022-01-22 09:10:10 -0800

    add includ/splinterdb/util.h

---
+++ include/splinterdb/util.h
@@ -0,0 +1,16 @@
+#ifndef __UTIL_H
+#define __UTIL_H
+
+typedef struct writable_buffer writable_buffer;
+
+uint64
+writable_buffer_length(writable_buffer *wb);
+
+/* Allocates memory as needed. Returns TRUE on success. */
+bool
+writable_buffer_set_length(writable_buffer *wb, uint64 newlength);
+
+void *
+writable_buffer_data(writable_buffer *wb);
+
+#endif /* __UTIL_H */
