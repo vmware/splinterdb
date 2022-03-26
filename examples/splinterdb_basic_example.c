69b90f16bdc0269add8d36db90f3212f3647a9cb
Aditya Gurajada <agurajada@vmware.com>
2022-03-26 14:18:05 +0000

    Add Makefile rules to compile splinterdb_basic_example.c

---
+++ examples/splinterdb_basic_example.c
@@ -0,0 +1,19 @@
+// Copyright 2022 VMware, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+/*
+ * SplinterDB Basic Example Program.
+ */
+
+#include <stdio.h>
+
+/*
+ * main() Driver for basic SplinterDB example program.
+ */
+int
+main()
+{
+   printf("     **** SplinterDB Basic Example program ****\n");
+   printf("Hello world\n");
+   return 0;
+}
