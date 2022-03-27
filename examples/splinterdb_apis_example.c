6210e0a2ae26f847c94ad08e56f3f54c27af0e07
Aditya Gurajada <agurajada@vmware.com>
2022-03-27 04:57:45 +0000

    Cleanup.

--- examples/splinterdb_apis_example.c
+++ examples/splinterdb_apis_example.c
@@ -13,7 +13,7 @@
 #include "example_common.h"
 
 /* Tag to identify messages from application program */
-#define APP_ME "App"
+#define APP_ME "App-APIs"
 
 /*
  * App-specific 'defaults' that can be parameterized, eventually.
@@ -26,6 +26,12 @@
 /* Application declares the limit of key-sizes it intends to use */
 #define APP_MAX_KEY_SIZE ((int)100)
 
+// Declare a struct to build a key/value pair
+typedef struct kv_pair {
+   char *kv_key;
+   char *kv_val;
+} kv_pair;
+
 // Function Prototypes
 void
 configure_splinter_instance(splinterdb_config *splinterdb_cfg,
@@ -33,6 +39,19 @@ configure_splinter_instance(splinterdb_config *splinterdb_cfg,
                             const char        *filename,
                             uint64             dev_size,
                             uint64             cache_size);
+
+void
+do_inserts_and_lookups(splinterdb *spl_handle);
+
+int
+do_insert(splinterdb  *spl_handle,
+          const char  *key_data,
+          const size_t key_len,
+          const char  *value_data,
+          const size_t value_len);
+
+int
+do_lookup(splinterdb *spl_handle, const char *key_data, const size_t key_len);
 
 /*
  * -------------------------------------------------------------------------------
@@ -65,6 +84,8 @@ main()
    }
    ex_msg("Created SplinterDB instance, dbname '%s'.\n\n", APP_DB_NAME);
 
+   do_inserts_and_lookups(spl_handle);
+
    splinterdb_close(&spl_handle);
    ex_msg("Shutdown SplinterDB instance, dbname '%s'.\n\n", APP_DB_NAME);
 
@@ -92,4 +113,91 @@ configure_splinter_instance(splinterdb_config *splinterdb_cfg,
    splinterdb_cfg->cache_size = cache_size;
    splinterdb_cfg->data_cfg   = splinter_data_cfg;
    return;
+}
+
+/*
+ * ---------------------------------------------------------------------------
+ * do_inserts_and_lookups()
+ *
+ * Insert a small number of key-value pairs. Perform few lookups.
+ */
+void
+do_inserts_and_lookups(splinterdb *spl_handle)
+{
+   int num_kv_pairs = 3;
+   // clang-format off
+   // Insert a set of key/value pairs
+   kv_pair kv_pairs[] = {
+                            { "k-1"       , "value-1" },
+                            { "key-2"     , "value-2" },
+                            { "key-third" , "3rd key's value: March" },
+                        };
+   // clang-format on
+
+   for (int ictr = 0; ictr < num_kv_pairs; ictr++) {
+      do_insert(spl_handle,
+                kv_pairs[ictr].kv_key,
+                strlen(kv_pairs[ictr].kv_key),
+                kv_pairs[ictr].kv_val,
+                strlen(kv_pairs[ictr].kv_val));
+   }
+
+   // Lookup a few keys that are known to exist
+   for (int ictr = 0; ictr < num_kv_pairs; ictr++) {
+      const char *key_data = kv_pairs[ictr].kv_key;
+      size_t      key_len  = strlen(kv_pairs[ictr].kv_key);
+      do_lookup(spl_handle, key_data, key_len);
+   }
+
+   do_lookup(spl_handle, "key-not-found", sizeof("key-not-found"));
+}
+
+/*
+ * ---------------------------------------------------------------------------
+ * do_insert()
+ *
+ * Insert a new key/value pair to a SplinterDB instance.
+ * ----------------------------------------------------------------------------
+ */
+int
+do_insert(splinterdb  *spl_handle,
+          const char  *key_data,
+          const size_t key_len,
+          const char  *value_data,
+          const size_t value_len)
+{
+   slice key   = slice_create(key_len, key_data);
+   slice value = slice_create(value_len, value_data);
+   int   rc    = splinterdb_insert(spl_handle, key, value);
+   return rc;
+}
+
+/*
+ * ---------------------------------------------------------------------------
+ * do_lookup()
+ *
+ * Lookup a value given a key.
+ * ---------------------------------------------------------------------------
+ */
+int
+do_lookup(splinterdb *spl_handle, const char *key_data, const size_t key_len)
+{
+   splinterdb_lookup_result result;
+   splinterdb_lookup_result_init(spl_handle, &result, 0, NULL);
+
+   slice key = slice_create(key_len, key_data);
+   int   rc  = splinterdb_lookup(spl_handle, key, &result);
+
+   slice value;
+   rc = splinterdb_lookup_result_value(spl_handle, &result, &value);
+   if (!rc) {
+      ex_msg("Found key: '%.*s', value: '%.*s'.\n",
+             (int)key_len,
+             key_data,
+             (int)slice_length(value),
+             (char *)slice_data(value));
+   } else {
+      ex_err("Did not find key '%.*s'.\n", (int)key_len, key_data);
+   }
+   return rc;
 }
