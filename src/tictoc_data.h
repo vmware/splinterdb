d51d0102e5f449c6495d90bc8aa7f92c74be549d
Deukyeon Hwang <hdeukyeon@vmware.com>
2022-07-20 05:14:37 -0700

    added the implementation of update and delete

--- src/tictoc_data.h
+++ src/tictoc_data.h
@@ -7,14 +7,17 @@
 
 #define TIMESTAMP_SIZE sizeof(uint32)
 
-typedef struct TS_word {
+typedef struct ONDISK TS_word {
    uint32 rts;
    uint32 wts;
 } TS_word;
 
-typedef struct tictoc_tuple {
+extern TS_word ZERO_TS_WORD;
+
+typedef struct ONDISK tictoc_tuple {
    TS_word ts_word;
-   char    value[]; // value provided by application
+   // char absent; // to indicate whether this tuple is deleted or not
+   char value[]; // value provided by application
 } tictoc_tuple;
 
 typedef enum entry_type {
@@ -26,6 +29,7 @@ typedef enum entry_type {
 // read_set and write_set entry stored locally
 typedef struct entry {
    entry_type      type;
+   message_type    op;
    slice           key;
    writable_buffer tuple;
 } entry;
