a1833060823a50042efcf383532724db6627e843
Gabriel Rosenhouse <grosenhouse@vmware.com>
2022-09-01 20:24:12 +0000

    Allow overriding MAX_KEY_SIZE via CFLAGS

--- include/splinterdb/limits.h
+++ include/splinterdb/limits.h
@@ -1,17 +1,28 @@
 // Copyright 2018-2021 VMware, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-/*
- * limits.h --
- *
- *     This file contains constants and functions the pertain to
- *     limits of keys and messages supported by the Key Value Store.
- */
-
 #ifndef __LIMITS_H__
 #define __LIMITS_H__
 
-#define MAX_KEY_SIZE    105 /* bytes */
-#define MAX_KEY_STR_LEN 128 /* bytes */
+// MAX_KEY_SIZE is the limit, in bytes, on the length of *raw* keys
+// inserted into the trunk and branches.
+//
+// It must be at least 8, and no more than 105.
+//
+// When using the splinterdb.h API, the key size limit is MAX_KEY_SIZE-1
+// to accomodate the variable-length encoding.
+// See include/splinterdb/splinterdb.h for details.
+//
+// If your keys are always shorter than 105 bytes, you may reduce MAX_KEY_SIZE.
+//
+// To modify this at build time without editing this file, set it via
+// the CFLAGS env var, e.g.
+//   make clean && CFLAGS='-DMAX_KEY_SIZE=24' make
+//
+// Tests and example programs use test-data with keys of varying sizes.
+// When building with a smaller MAX_KEY_SIZE, expect some breakage.
+#ifndef MAX_KEY_SIZE
+#   define MAX_KEY_SIZE 105
+#endif // MAX_KEY_SIZE
 
 #endif // __LIMITS_H__
