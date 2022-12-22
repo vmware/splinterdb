156a376acf7f151a244b39bba78a335756a980bc
Jon Howell <howell@vmware.com>
2022-12-21 16:56:28 -0800

    unsafe rust stubcache can allocate page handles

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,9 @@
 use libc;
 use libc::c_void;
-use splinterdb::{allocator, cache, cache_config, cache_config_ops, page_handle, page_type, platform_status , cache_ops };
+use splinterdb::{
+    allocator, cache, cache_config, cache_config_ops, cache_ops, page_handle, page_type,
+    platform_status,
+};
 
 /// cbindgen:ignore
 mod splinterdb;
@@ -38,20 +41,18 @@ pub unsafe extern "C" fn stubcache_config_init(
     extent_size: u64,
     al: &mut allocator,
 ) {
-   cfg.super_.ops = &stubcache_config_ops;
-   //let ops = &(*(al.ops));
-   cfg.disk_capacity_bytes = (*(al.ops)).get_capacity.unwrap()(al); 
-       // kinda evil, but we can't link to static inlines in allocator.h,
+    cfg.super_.ops = &stubcache_config_ops;
+    //let ops = &(*(al.ops));
+    cfg.disk_capacity_bytes = (*(al.ops)).get_capacity.unwrap()(al);
+    // kinda evil, but we can't link to static inlines in allocator.h,
 
+    cfg.page_size = page_size;
+    assert!(cfg.page_size == 4096);
+    cfg.disk_capacity_pages = cfg.disk_capacity_bytes / cfg.page_size;
 
-   cfg.page_size = page_size;
-   assert!(cfg.page_size == 4096);
-   cfg.disk_capacity_pages = cfg.disk_capacity_bytes / cfg.page_size;
-
-   cfg.extent_size = extent_size;
-   let log_extent_size: u64   = 63u64 - (extent_size.leading_zeros() as u64);
-   cfg.extent_mask   = !((1u64 << log_extent_size) - 1);
-
+    cfg.extent_size = extent_size;
+    let log_extent_size: u64 = 63u64 - (extent_size.leading_zeros() as u64);
+    cfg.extent_mask = !((1u64 << log_extent_size) - 1);
 }
 
 #[repr(C)]
@@ -59,18 +60,22 @@ pub struct stubcache {
     super_: cache,
     cfg: *mut stubcache_config,
     al: *mut allocator,
-    page_handles: *mut page_handle,
-    the_disk: *mut char,
+    page_handles: Vec<page_handle>,
+    the_disk: *mut i8,
 }
 
 /// Probably unsafe.
-#[no_mangle]
-pub unsafe extern "C" fn stubcache_page_alloc(
-    sc: *mut stubcache,
+unsafe extern "C" fn stubcache_page_alloc(
+    cc: *mut cache,
     addr: u64,
     type_: page_type,
 ) -> *mut page_handle {
-    return std::ptr::null_mut();
+    /*
+    return (*sc).the_disk.add(addr as usize) as page_handle;
+    */
+    //return std::ptr::null_mut();
+    let sc = cc as *mut stubcache;
+    return &mut ((*sc).page_handles[(addr / (*(*sc).cfg).page_size) as usize]);
 }
 
 fn unimpl() {
@@ -78,63 +83,71 @@ fn unimpl() {
 }
 
 const stubcache_ops: cache_ops = cache_ops {
-   page_alloc : None,
+    page_alloc: Some(stubcache_page_alloc),
 
-   extent_hard_evict : None,
-   page_get : None,
-   page_get_async : None,
-   page_async_done : None,
-   page_unget : None,
-   page_claim : None,
-   page_unclaim : None,
-   page_lock : None,
-   page_unlock : None,
-   page_prefetch : None,
-   page_mark_dirty : None,
-   page_pin : None,
-   page_unpin : None,
-   page_sync : None,
-   extent_sync : None,
-   flush : None,
-   evict : None,
-   cleanup : None,
-   assert_ungot : None,
-   assert_free : None,
-   validate_page : None,
-   cache_present : None,
-   print : None,
-   print_stats : None,
-   io_stats : None,
-   reset_stats : None,
-   count_dirty : None,
-   page_get_read_ref : None,
-   enable_sync_get : None,
-   get_allocator : None,
-   get_config : None,
+    extent_hard_evict: None,
+    page_get: None,
+    page_get_async: None,
+    page_async_done: None,
+    page_unget: None,
+    page_claim: None,
+    page_unclaim: None,
+    page_lock: None,
+    page_unlock: None,
+    page_prefetch: None,
+    page_mark_dirty: None,
+    page_pin: None,
+    page_unpin: None,
+    page_sync: None,
+    extent_sync: None,
+    flush: None,
+    evict: None,
+    cleanup: None,
+    assert_ungot: None,
+    assert_free: None,
+    validate_page: None,
+    cache_present: None,
+    print: None,
+    print_stats: None,
+    io_stats: None,
+    reset_stats: None,
+    count_dirty: None,
+    page_get_read_ref: None,
+    enable_sync_get: None,
+    get_allocator: None,
+    get_config: None,
 };
 
 #[no_mangle]
 pub unsafe extern "C" fn stubcache_init(
     sc: &mut stubcache,
     cfg: &mut stubcache_config,
-     al: &mut allocator) -> platform_status
-{
-   sc.super_.ops = &stubcache_ops;
-   sc.cfg = cfg;
-   sc.al = al;
+    al: &mut allocator,
+) -> platform_status {
+    sc.super_.ops = &stubcache_ops;
+    sc.cfg = cfg;
+    sc.al = al;
 
-   // platform_heap_id hid_0 = 0;
-   sc.page_handles = std::ptr::null_mut();
-   //sc.page_handles = TYPED_ARRAY_MALLOC(hid_0, sc->page_handles, sc->cfg->disk_capacity_pages);
-   sc.the_disk = libc::malloc(std::mem::size_of::<char>() * (*(sc.cfg)).disk_capacity_bytes as usize) as *mut char;
+    // platform_heap_id hid_0 = 0;
+    sc.page_handles = Vec::with_capacity(cfg.disk_capacity_pages as usize);
+    //sc.page_handles.set_len(cfg.disk_capacity_pages as usize);
+    sc.the_disk =
+        libc::malloc(std::mem::size_of::<char>() * (*(sc.cfg)).disk_capacity_bytes as usize)
+            as *mut i8;
 
-   let STATUS_OK = platform_status { r : 0 };
-   return STATUS_OK;
+    for i in 0..cfg.disk_capacity_pages {
+        let addr = i * cfg.page_size;
+        sc.page_handles.push(page_handle {
+            data: sc.the_disk.add(addr as usize),
+            disk_addr: addr,
+        });
+    }
+
+    let STATUS_OK = platform_status { r: 0 };
+    return STATUS_OK;
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn stubcache_deinit(sc: &mut stubcache)
-{
+pub unsafe extern "C" fn stubcache_deinit(sc: &mut stubcache) {
     libc::free(sc.the_disk as *mut c_void);
 }
-
