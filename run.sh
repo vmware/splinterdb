98d94671f139cb21c820fe8badfc197ed37b2c11
Rob Johnson <rob@robjohnson.io>
2023-01-09 14:52:02 -0800

    finish conversion to tabular approach

--- run.sh
+++ run.sh
@@ -2,37 +2,6 @@
 
 set -ex
 
-# EVAL=$1
-
-# if [ $EVAL == 'iso' ]; then
-# 	DRAM_CAPACITY=(6144 4096 2048 8192)
-# 	PMEM_CAPACITY=(8192 16384 24576 0)
-# else
-# 	DRAM_CAPACITY="4096"
-# 	PMEM_CAPACITY="16384"
-# fi
-
-# if [ $EVAL == 'workload' ]; then
-# 	WORKLOAD="load workloada workloadb workloadc workloadd workloade workloadf"
-# else
-# 	WORKLOAD="load workloada workloadb"
-# fi
-
-# if [ $EVAL == 'threading' ]; then
-#         THREADS="1 2 4 8 16"
-# else
-#         THREADS="16"
-# fi
-
-# if [ $EVAL == 'sys-compare' ]; then
-#         BENCHMARKS="SplinterDB-withLog SplinterDB PMEM-Only PMEM-CoW Non-Txn PERSISTRON"
-# elif [ $EVAL ==  'threading' ]; then
-# 	BENCHMARKS="SplinterDB PERSISTRON"
-# else
-# 	#FIXME: build and run matrixkv & spitfire
-#         BENCHMARKS="SplinterDB PERSISTRON MatrixKV Spitfire"
-# fi
-
 if [ "$FORCE" == "" ]; then
     FORCE=0
 fi
@@ -40,9 +9,6 @@ fi
 ITERATIONS=5
 
 PMEM_ROOT=/tmp/atc23-persistron
-
-SPLINTER_DIR=.
-YCSBC_DIR=../YCSB-C
 
 function workload_args () {
     local result=
@@ -61,6 +27,9 @@ function run_if_needed () {
         $cmd > "$file" 2>&1
     fi
 }
+
+SPLINTER_DIR=.
+YCSBC_DIR=../YCSB-C
 
 export COMPILER=gcc
 export CC=$COMPILER
@@ -173,148 +142,3 @@ build_splinterdb_ycsb "splinterdb-withlog" "-DDRAM_CACHE -DPAGE_MIGRATION -DEVIC
 execute_splinterdb_ycsb "splinterdb-withlog"       8        0                 16384           10000               a c
 
 
-exit
-
-{
-for B in $BENCHMARKS; do
-    case $B in
-        "SplinterDB")
-            echo "Running SplinterDB"
-            ;;
-        "PMEM-Only")
-            echo "Running PMEM-Only"
-            ;;
-        "PMEM-CoW")
-            echo "Running PMEM-CoW"
-            DEFAULT_CFLAGS=-DPMEM_COW
-            ;;
-        "Non-Txn")
-            echo "Running Non-Txn"
-            DEFAULT_CFLAGS=-DPMEM_COW -DNON_TX_OPT
-            ;;
-        "PERSISTRON")
-            echo "Running PERSISTRON"
-            DEFAULT_CFLAGS=-DPMEM_COW -DNON_TX_OPT -DDRAM_CACHE -DPAGE_MIGRATION -DEVICTION_OPT
-            ;;
-        "SplinterDB-withLog")
-            echo "Running SplinterDB-withLog"
-            DEFAULT_CFLAGS=-DDRAM_CACHE -DPAGE_MIGRATION -DEVICTION_OPT -DLOG_CHECKPOINT
-            ;;
-        *)
-            echo "Not a valid argument"
-            echo
-            ;;
-    esac
-    make clean
-    make
-    sudo make install
-
-    cd ../YCSB-C/
-    make clean
-    make -j18
-
-
-    for W in $WORKLOAD; do
-        for T in $THREADS; do
-	          for C in `seq 1 ${#DRAM_CAPACITY[@]}`; do
-    		        for I in `seq 0 4`; do
-                	  rm -rf ${MOUNT_POINT}/*
-			              rm splinterdb.db
-			              if [ $W == load ]; then
-				                if [ $B == SplinterDB ]; then
-					                  if [[ ${#DRAM_CAPACITY[@]} == 1 || ${PMEM_CAPACITY[$C-1]} == 0 ]]; then
-			        		              ./ycsbc -db classic_splinterdb -L workloads/$W.spec -threads $T \
-							                          -dram_cache_size_mb  ${DRAM_CAPACITY[$C-1]} >&data.log
-						                    RATE=`cat data.log | grep 'workloads/load.spec' | awk '{ print $4 }'`
-						                    echo "${B},${W},${T},${PMEM_CAPACITY[$C-1]},${DRAM_CAPACITY[$C-1]},${I},${RATE}">> result.csv
-					                  fi
-				                elif [ $B == SplinterDB-withLog ]; then
-					                  if [ ${PMEM_CAPACITY[$C-1]} != 0 ]; then
-			        		              ./ycsbc -db classic_splinterdb -L workloads/$W.spec -threads $T \
-							                          -pmem_cache_size_mb ${PMEM_CAPACITY[$C-1]} \
-                         				        -dram_cache_size_mb  ${DRAM_CAPACITY[$C-1]}\
-							                          -cache_log_checkpoint_interval 10000\
-							                          -pmem_cache_file ${MOUNT_POINT}/pmemcache \
-                         				        >&data.log
-						                    RATE=`cat data.log | grep 'workloads/load.spec' | awk '{ print $4 }'`
-						                    echo "${B},${W},${T},${PMEM_CAPACITY[$C-1]},${DRAM_CAPACITY[$C-1]},${I},${RATE}">> result.csv
-					                  fi
-				                else
-					                  if [ ${PMEM_CAPACITY[$C-1]} != 0 ]; then
-						                    ./ycsbc -db classic_splinterdb -L workloads/$W.spec -threads $T \
-							                          -pmem_cache_size_mb ${PMEM_CAPACITY[$C-1]} \
-							                          -dram_cache_size_mb  ${DRAM_CAPACITY[$C-1]}\
-							                          -pmem_cache_file ${MOUNT_POINT}/pmemcache >&data.log
-						                    RATE=`cat data.log | grep 'workloads/load.spec' | awk '{ print $4 }'`
-						                    echo "${B},${W},${T},${PMEM_CAPACITY[$C-1]},${DRAM_CAPACITY[$C-1]},${I},${RATE}">> result.csv
-					                  fi
-				                fi
-			              else
-				                if [ $B == SplinterDB ]; then
-					                  if [[ ${#DRAM_CAPACITY[@]} == 1 || ${PMEM_CAPACITY[$C-1]} == 0 ]]; then
-						                    ./ycsbc -db classic_splinterdb -L workloads/load.spec -W workloads/$W.spec -threads $T \
-							                          -dram_cache_size_mb ${DRAM_CAPACITY[$C-1]} >&data.log
-						                    RATE=`cat data.log | grep 'workloads/workload' | awk '{ print $4 }'`
-						                    echo "${B},${W},${T},${PMEM_CAPACITY[$C-1]},${DRAM_CAPACITY[$C-1]},${I},${RATE}">> result.csv
-					                  fi
-				                elif [ $B == SplinterDB-withLog ]; then
-					                  if [ ${PMEM_CAPACITY[$C-1]} != 0 ]; then
-						                    ./ycsbc -db classic_splinterdb -L workloads/load.spec -W workloads/$W.spec -threads $T \
-							                          -pmem_cache_size_mb ${PMEM_CAPACITY[$C-1]}\
-					       		                    -dram_cache_size_mb ${DRAM_CAPACITY[$C-1]}\
-							                          -cache_log_checkpoint_interval 10000\
-							                          -pmem_cache_file ${MOUNT_POINT}/pmemcache >&data.log
-						                    RATE=`cat data.log | grep 'workloads/workload' | awk '{ print $4 }'`
-						                    echo "${B},${W},${T},${PMEM_CAPACITY[$C-1]},${DRAM_CAPACITY[$C-1]},${I},${RATE}">> result.csv
-					                  fi
-				                else
-					                  if [ ${PMEM_CAPACITY[$C-1]} != 0 ]; then
-						                    ./ycsbc -db classic_splinterdb -L workloads/load.spec -W workloads/$W.spec -threads $T \
-							                          -pmem_cache_size_mb ${PMEM_CAPACITY[$C-1]} \
-						       	                    -dram_cache_size_mb ${DRAM_CAPACITY[$C-1]} \
-							                          -pmem_cache_file ${MOUNT_POINT}/pmemcache >&data.log
-						                    RATE=`cat data.log | grep 'workloads/workload' | awk '{ print $4 }'`
-						                    echo "${B},${W},${T},${PMEM_CAPACITY[$C-1]},${DRAM_CAPACITY[$C-1]},${I},${RATE}">> result.csv
-					                  fi
-				                fi
-			              fi
-                done
-	          done
-        done
-    done
-
-    cd ../splinterdb
-
-    case $B in
-        "SplinterDB")
-            echo "Finished running SplinterDB"
-            ;;
-        "PMEM-Only")
-            echo "Finished running PMEM-Only"
-            #sed -i -e 's:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS):DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS):g' Makefile
-            ;;
-        "PMEM-CoW")
-            echo "Finished running PMEM-CoW"
-            #sed -i -e 's:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS) -DPMEM_COW:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS):g' Makefile
-            ;;
-        "Non-Txn")
-            echo "Finished running Non-Txn"
-            #sed -i -e 's:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS) -DPMEM_COW -DNON_TX_OPT:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS):g' Makefile
-            ;;
-        "PERSISTRON")
-            echo "Finished running PERSISTRON"
-            #sed -i -e 's:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS) -DPMEM_COW -DNON_TX_OPT -DDRAM_CACHE -DPAGE_MIGRATION -DEVICTION_OPT:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS):g' Makefile
-            ;;
-        "SplinterDB-withLog")
-            echo "Finished running SplinterDB-withLog"
-            #sed -i -e 's:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS) -DDRAM_CACHE -DPAGE_MIGRATION -DEVICTION_OPT -DLOG_CHECKPOINT:DEFAULT_CFLAGS += $(LIBCONFIG_CFLAGS):g' Makefile
-            ;;
-        *)
-            echo "Not a valid argument"
-            echo
-            ;;
-    esac
-done
-} 
->&output.log
-rm output.log
