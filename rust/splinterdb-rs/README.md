497a96678253a8e24935c3ab73ebce661339428d
Evan West <evan.ts.west@gmail.com>
2023-05-27 19:50:01 +0000

    SplinterDB rust wrapper: add documentation

---
+++ rust/splinterdb-rs/README.md
@@ -0,0 +1,6 @@
+# `splinterdb-rs`
+
+This crate aims to be a safe and ergonomic Rust wrapper SplinterDB's public API.
+
+Currently, it exposes a simple key/value abstraction, by using the `splinterdb` and
+`default_data_config` modules.
--- rust/splinterdb-sys/wrapper.h
+++ rust/splinterdb-sys/wrapper.h
@@ -1,6 +1,5 @@
 // This file lists the C headers that bindgen will process
 // when creating the rust interface to splinterdb
-// See regenerate.sh for details
 
 #include <splinterdb/splinterdb.h>
 #include <splinterdb/default_data_config.h>
