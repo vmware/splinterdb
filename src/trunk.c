31053d07b13aaa7412ddc7e8f9c807b149bcbfad
aaditya2200 <aaditya2200@gmail.com>
2024-04-09 12:22:26 -0600

    Revert "Small refactor for debug"
    This reverts commit c575965daa10b2d80e9c3341db8e1a11c17ec864.

--- src/trunk.c
+++ src/trunk.c
@@ -6738,7 +6738,6 @@ trunk_lookup(trunk_handle *spl, key target, merge_accumulator *result, slice nod
     trunk_node node;
     trunk_node temp;
     trunk_root_get(spl, &node);
-    trunk_root_get(spl, &temp);
 
     // release memtable lookup lock
     memtable_end_lookup(spl->mt_ctxt);
@@ -6771,7 +6770,11 @@ trunk_lookup(trunk_handle *spl, key target, merge_accumulator *result, slice nod
 
         //! Check if messages are needed to be flushed.
         if (trunk_pivot_needs_flush(spl, &node, pdata, 0)) {
-
+            if (node.addr == spl->root_addr) {
+            temp = node;
+        } else {
+            trunk_root_get(spl, &temp);
+        }
         //! Take claims and locks
         trunk_node_claim(spl->cc, &temp);
         //! Get write lock on parent
@@ -6780,14 +6783,12 @@ trunk_lookup(trunk_handle *spl, key target, merge_accumulator *result, slice nod
            trunk_node_unlock(spl->cc, &node);
         trunk_node_unclaim(spl->cc, &temp);
         trunk_node_unget(spl->cc, &temp);
-
-        //! Continue
-          if (temp.addr != node.addr) {
-            trunk_node_unget(spl->cc, &node);
-        }
         }
 #endif
-
+        //! Continue
+        if (temp.addr != node.addr) {
+            trunk_node_unget(spl->cc, &node);
+        }
         node = child;
     }
 
