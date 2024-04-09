cda3a0c0d4e97c0d2a5b632f38864b94897eec89
aaditya2200 <aaditya2200@gmail.com>
2024-04-09 15:08:14 -0600

    Revert "Trying something new"
    This reverts commit 88ef835addf6889560ff39e6583d9dd4a347f4ba.

--- src/trunk.c
+++ src/trunk.c
@@ -6780,27 +6780,23 @@ trunk_lookup(trunk_handle *spl, key target, merge_accumulator *result, slice nod
         //! Claim on root
         //! Check if messages are needed to be flushed.
         if (trunk_pivot_needs_flush(spl, &node, pdata, 0)) {
-//            if (node.addr == spl->root_addr) {
-//                //! Just lock the root node, no need to do anything else.
-//                trunk_node_claim(spl->cc, &node);
-//                trunk_node_lock(spl->cc, &node);
-//            } else {
-//                trunk_root_get(spl, &temp);
-//                trunk_node_claim(spl->cc, &temp);
-//                trunk_node_lock(spl->cc, &node);
-//            }
-            trunk_root_full_claim(spl);
-            trunk_node_lock(spl->cc, &node);
+            if (node.addr == spl->root_addr) {
+                //! Just lock the root node, no need to do anything else.
+                trunk_node_claim(spl->cc, &node);
+                trunk_node_lock(spl->cc, &node);
+            } else {
+                trunk_root_get(spl, &temp);
+                trunk_node_claim(spl->cc, &temp);
+                trunk_node_lock(spl->cc, &node);
+            }
             trunk_flush(spl, &node, pdata, FALSE);
-//            if (node.addr == spl->root_addr) {
-//                trunk_node_unclaim(spl->cc, &node);
-//                trunk_node_unlock(spl->cc, &node);
-//            } else {
-//                trunk_node_unlock(spl->cc, &node);
-//                trunk_node_unget(spl->cc, &temp);
-//            }
-            trunk_node_unlock(spl->cc, &node);
-            trunk_root_full_unclaim(spl);
+            if (node.addr == spl->root_addr) {
+                trunk_node_unclaim(spl->cc, &node);
+                trunk_node_unlock(spl->cc, &node);
+            } else {
+                trunk_node_unlock(spl->cc, &node);
+                trunk_node_unget(spl->cc, &temp);
+            }
         }
         trunk_node_unget(spl->cc, &node);
         node = child;
