73ccc36bdc3d97e2a2bed2fe35b53e6ffe3776f4
Adriana Szekeres <aszekeres@vmware.com>
2024-07-17 15:35:59 -0700

    Fix 2

--- src/transaction_impl/transaction_mvcc_memory.h
+++ src/transaction_impl/transaction_mvcc_memory.h
@@ -142,8 +142,8 @@ version_meta_try_wrlock(version_meta *meta, txn_timestamp ts)
       return VERSION_LOCK_STATUS_ABORT;
    }
 
-   platform_default_log("lock check %d\n", meta->lock);
    mvcc_lock l = meta->lock; //atomically read the lock's lock_bit and holder (64-bit aligned)
+   platform_default_log("lock check %d\n", *(uint64_t *)&l);
    if (l && l.lock_holder > ts) {
       // a transaction with higher timestamp already holds this lock
       // so we abort to prevent deadlocks
