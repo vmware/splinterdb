189d784e26743688290fc2b9c6267064c15bbb4e
Rob Johnson <rob@robjohnson.io>
2026-06-03 13:05:50 -0700

    shrink a few tests due to shrinking disk space on github runners
    Signed-off-by: Rob Johnson <rob@robjohnson.io>

--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@ set -e
 
 source timing_utils.sh
 
+DB_CAPACITY="--db-capacity-gib 20"
+
 function run()
 {
-    run_with_timing "$*" ${BINDIR}/$@
+    run_with_timing "$*" ${BINDIR}/$@ $DB_CAPACITY
     rm -f db
 }
 
@@ -14,7 +16,7 @@ function run()
 function cache_tests_1() {
     # 25 sec each
     run driver_test cache_test --perf
-    run driver_test cache_test --perf                                             --use-shmem
+    run driver_test cache_test --perf     --use-shmem
     # 3 sec each
     run driver_test cache_test --seed 135
     run driver_test cache_test --seed 135 --use-shmem
@@ -26,12 +28,12 @@ function cache_tests_1() {
 
 function cache_tests_2() {
     # 390 sec each
-    run driver_test cache_test --perf --cache-capacity-gib 6 --db-capacity-gib 60
+    run driver_test cache_test --perf --cache-capacity-gib 4
 }
 
 function cache_tests_3() {
         # 390 sec each
-    run driver_test cache_test --perf --cache-capacity-gib 6 --db-capacity-gib 60 --use-shmem
+    run driver_test cache_test --perf --cache-capacity-gib 4 --use-shmem
 }
 
 # 12 minutes
@@ -54,43 +56,43 @@ function functionality_tests() {
 
 function parallel_perf_test_1() {
     # 115 sec each
-    run driver_test splinter_test --parallel-perf --max-async-inflight  0 --num-pthreads  8 --lookup-positive-percent 10 --tree-size-gib 8                                                        --db-capacity-gib 60
+    run driver_test splinter_test --parallel-perf --max-async-inflight  0 --num-pthreads  8 --lookup-positive-percent 10 --tree-size-gib 8                                                        
 }
 
 function parallel_perf_test_2() {
     # 115 sec each
-    run driver_test splinter_test --parallel-perf --max-async-inflight  0 --num-pthreads  8 --lookup-positive-percent 10 --tree-size-gib 8                                                        --db-capacity-gib 60 --use-shmem
+    run driver_test splinter_test --parallel-perf --max-async-inflight  0 --num-pthreads  8 --lookup-positive-percent 10 --tree-size-gib 8                                                        --use-shmem
 }
 
 function parallel_perf_test_3() {
     # 115 sec each
-    run driver_test splinter_test --parallel-perf --max-async-inflight 10 --num-pthreads 20                              --tree-size-gib 5 --num-normal-bg-threads 20 --num-memtable-bg-threads 2 --db-capacity-gib 60
+    run driver_test splinter_test --parallel-perf --max-async-inflight 10 --num-pthreads 20                              --tree-size-gib 5 --num-normal-bg-threads 20 --num-memtable-bg-threads 2 
 }
 
 function parallel_perf_test_4() {
     # 115 sec each
-    run driver_test splinter_test --parallel-perf --max-async-inflight 10 --num-pthreads 20                              --tree-size-gib 5 --num-normal-bg-threads 20 --num-memtable-bg-threads 2 --db-capacity-gib 60
+    run driver_test splinter_test --parallel-perf --max-async-inflight 10 --num-pthreads 20                              --tree-size-gib 5 --num-normal-bg-threads 20 --num-memtable-bg-threads 2 
 }
 
 
 # 10 minutes
 function perf_tests_1() {
     # 60 sec each
-    run driver_test splinter_test --perf                                    --num-insert-threads 4  --num-lookup-threads 4                                                                             --num-inserts 10000 --cache-capacity-mib 512                                         --num-normal-bg-threads 1 --num-memtable-bg-threads 1
+    run driver_test splinter_test --perf                                    --num-insert-threads 4  --num-lookup-threads 4                                                                             --num-inserts 10000 --cache-capacity-mib 512                    --num-normal-bg-threads 1 --num-memtable-bg-threads 1
     run driver_test splinter_test --perf                                    --num-insert-threads 63                        --num-range-lookup-threads 0                              --tree-size-gib 1
     run driver_test splinter_test --perf             --max-async-inflight 0 --num-insert-threads 4  --num-lookup-threads 4 --num-range-lookup-threads 0                              --tree-size-gib 2                     --cache-capacity-mib 512
-    run driver_test splinter_test --perf             --max-async-inflight 0 --num-insert-threads 4  --num-lookup-threads 4 --num-range-lookup-threads 4 --lookup-positive-percent 10                   --num-inserts 10000 --cache-capacity-mib 512                      --verbose-progress
-    run driver_test splinter_test --perf             --max-async-inflight 0 --num-insert-threads 8  --num-lookup-threads 8 --num-range-lookup-threads 8 --lookup-positive-percent 10 --tree-size-gib 4                                              --db-capacity-gib 60 --verbose-progress
+    run driver_test splinter_test --perf             --max-async-inflight 0 --num-insert-threads 4  --num-lookup-threads 4 --num-range-lookup-threads 4 --lookup-positive-percent 10                   --num-inserts 10000 --cache-capacity-mib 512 --verbose-progress
+    run driver_test splinter_test --perf             --max-async-inflight 0 --num-insert-threads 8  --num-lookup-threads 8 --num-range-lookup-threads 8 --lookup-positive-percent 10 --tree-size-gib 4                                              --verbose-progress
 }
 
 # 10 minutes
 function perf_tests_2() {
     # 60 sec each
-    run driver_test splinter_test --perf --use-shmem                        --num-insert-threads 4  --num-lookup-threads 4                                                                             --num-inserts 10000 --cache-capacity-mib 512                                         --num-normal-bg-threads 1 --num-memtable-bg-threads 1
+    run driver_test splinter_test --perf --use-shmem                        --num-insert-threads 4  --num-lookup-threads 4                                                                             --num-inserts 10000 --cache-capacity-mib 512                    --num-normal-bg-threads 1 --num-memtable-bg-threads 1
     run driver_test splinter_test --perf --use-shmem                        --num-insert-threads 63                        --num-range-lookup-threads 0                              --tree-size-gib 1
     run driver_test splinter_test --perf --use-shmem --max-async-inflight 0 --num-insert-threads 4  --num-lookup-threads 4 --num-range-lookup-threads 0                              --tree-size-gib 2                     --cache-capacity-mib 512
-    run driver_test splinter_test --perf --use-shmem --max-async-inflight 0 --num-insert-threads 4  --num-lookup-threads 4 --num-range-lookup-threads 4 --lookup-positive-percent 10                   --num-inserts 10000 --cache-capacity-mib 512                      --verbose-progress
-    run driver_test splinter_test --perf --use-shmem --max-async-inflight 0 --num-insert-threads 8  --num-lookup-threads 8 --num-range-lookup-threads 8 --lookup-positive-percent 10 --tree-size-gib 4                                              --db-capacity-gib 60 --verbose-progress
+    run driver_test splinter_test --perf --use-shmem --max-async-inflight 0 --num-insert-threads 4  --num-lookup-threads 4 --num-range-lookup-threads 4 --lookup-positive-percent 10                   --num-inserts 10000 --cache-capacity-mib 512 --verbose-progress
+    run driver_test splinter_test --perf --use-shmem --max-async-inflight 0 --num-insert-threads 8  --num-lookup-threads 8 --num-range-lookup-threads 8 --lookup-positive-percent 10 --tree-size-gib 4                                              --verbose-progress
 }
 
 # 2.5 minutes
@@ -110,20 +112,20 @@ function splinter_misc_tests_2()
 
 function large_insert_stress_tests_1() {
     # 25, 50 sec each
-    run unit/large_inserts_stress_test                                    --num-inserts  1000000
-    run unit/large_inserts_stress_test                                    --num-inserts  2000000 --num-normal-bg-threads  4 --num-memtable-bg-threads 3
-    run unit/large_inserts_stress_test --use-shmem                        --num-inserts  1000000
-    run unit/large_inserts_stress_test --use-shmem                        --num-inserts  2000000 --num-normal-bg-threads  4 --num-memtable-bg-threads 3
+    run unit/large_inserts_stress_test             --num-inserts  1000000
+    run unit/large_inserts_stress_test             --num-inserts  2000000 --num-normal-bg-threads  4 --num-memtable-bg-threads 3
+    run unit/large_inserts_stress_test --use-shmem --num-inserts  1000000
+    run unit/large_inserts_stress_test --use-shmem --num-inserts  2000000 --num-normal-bg-threads  4 --num-memtable-bg-threads 3
 }
 
 function large_insert_stress_tests_2() {
     # 250 sec
-    run unit/large_inserts_stress_test             --shmem-capacity-gib 8 --num-inserts 10000000 --num-normal-bg-threads 20 --num-memtable-bg-threads 8
+    run unit/large_inserts_stress_test             --num-inserts  8000000 --num-normal-bg-threads 20 --num-memtable-bg-threads 8
 }
 
 function large_insert_stress_tests_3() {
     # 250 sec
-    run unit/large_inserts_stress_test --use-shmem --shmem-capacity-gib 8 --num-inserts 10000000 --num-normal-bg-threads 20 --num-memtable-bg-threads 8
+    run unit/large_inserts_stress_test --use-shmem --num-inserts  8000000 --num-normal-bg-threads 20 --num-memtable-bg-threads 8
 }
 
 function filter_tests() {
@@ -165,14 +167,8 @@ function misc_tests() {
     run driver_test log_test --perf
 
     # 12 sec each
-    run unit/splinter_test             --num-inserts 2000000 test_lookups
-    run unit/splinter_test --use-shmem --num-inserts 2000000 test_lookups
-
-    # 2 sec each
-    run unit/splinter_test             test_inserts
-    run unit/splinter_test             test_splinter_print_diags
-    run unit/splinter_test --use-shmem test_inserts
-    run unit/splinter_test --use-shmem test_splinter_print_diags
+    run unit/splinter_test             --num-inserts 2000000
+    run unit/splinter_test --use-shmem --num-inserts 2000000
 }
 
 function all_tests() {
@@ -197,6 +193,7 @@ function all_tests() {
 }
 
 function main() {
+    echo > db.sizes.log
     if [ -z "$TESTS_FUNCTION" ]; then
         TESTS_FUNCTION="all_tests"
     fi
